006b346274502ca38d3ef2c497157d12e05adf63
Nikita Chulkov <nikita_chul@mail.ru>
2025-04-18 18:31:26 +1100

    [build.sh] Introduced `-b` and -`p` flag, passing flags like `-abc`

--- build.sh
+++ build.sh
@@ -1,50 +1,78 @@
 #!/bin/sh
 
-CFLAGS="-Wall -Wextra -pedantic -std=c23"
+CFLAGS="-Wall -Wextra -std=c11"
 CLIBS="-I. -lmd -lcrypto"
-CDEBUG="-ggdb -fsanitize=address -fno-omit-frame-pointer"
-CC="clang"
+CDEBUG="-ggdb -fsanitize=address -fno-omit-frame-pointer -D_LOGCIE_DEBUG"
+CC="gcc"
 OUTDIR="./out"
 VERBOSE=true
+BEAR=false
 
 usage() {
   echo "-d --debug     Compile with debug flags"
+  echo "-b --bear      Call bear to generate clang configuration"
+  echo "-p --pedantic  Add pedantic flags"
   echo "-s --silent    Compile without unnececary output"
   echo "-o --outdir    Set output dir (default: ./out)"
   echo "-c --compiler  Set which compier to use (default: clang)"
   echo "-h --help      Print help"
 }
 
-while [[ $# -gt 0 ]]; do
+handle_arg() {
   case "$1" in
-    "-d"|"--debug") CFLAGS="$CFLAGS $CDEBUG"
-      shift
-      ;;
-    "-s"|"--silent") VERBOSE=false
-      shift
-      ;;
-    "-c"|"--compiler") CC="$2"
-      shift
-      shift
-      ;;
-    "-o"|"--outdir") OUTDIR="$2"
-      shift
-      shift
-      ;;
-    "-h"|"--help") usage;
+    "${2}d"|"--debug") CFLAGS="$CFLAGS $CDEBUG"
+     shift
+     ;;
+    "${2}b"|"--bear") BEAR=true
+     shift
+     ;;
+    "${2}p"|"--pedantic") CFLAGS="$CFLAGS -pedantic -DLOGCIE_PEDANTIC"
+     shift
+     ;;
+    "${2}s"|"--silent") VERBOSE=false
+     shift
+     shift
+     ;;
+    "${2}c"|"--compiler") CC="$2"
+     shift
+     shift
+     ;;
+    "${2}o"|"--outdir") OUTDIR="$2"
+     shift
+     shift
+     ;;
+    "${2}h"|"--help") usage;
       shift
       exit 1;
       ;;
-    "") # pass through
+    ""|"-") # pass through
+      shift
       ;;
     *) echo "Unknown command '$1'";
-      shift
       usage;
       exit 1;
+  esac
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -*)
+      for (( i=0; i<${#1}; i++ )); do
+        handle_arg "${1:$i:1}" ''
+      done
+      shift
+      ;;
+    *) handle_arg $1 '-'
+      ;;
   esac
 done
 
 set -e
+
+# Insuring that flag -c would not undo flag -b
+if $BEAR; then
+  CC="bear -- $CC"
+fi
 
 if [ ! -d "$OUTDIR" ]; then
   mkdir -p "$OUTDIR";
@@ -54,4 +82,4 @@ if $VERBOSE; then
   set -x
 fi
 
-$CC $CFLAGS $CLIBS -o "$OUTDIR/test" ./test/test.c # ./test/module.c
+$CC $CFLAGS $CLIBS -o "$OUTDIR/test" ./examples/example.c ./examples/module.c ./examples/libs.c
