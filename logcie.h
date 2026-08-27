777681bb8d2900b4b21af18c301eff315dfb48ed
Nikita Chulkov <nikita_chul@mail.ru>
2026-08-27 12:08:27 +1100

    chore: bumped version to v1.2.1

--- logcie.h
+++ logcie.h
@@ -1,5 +1,5 @@
 /*
- * Logcie v1.2.0 - Logging Library (Single Header)
+ * Logcie v1.2.1 - Logging Library (Single Header)
  *
  * Description:
  *   Logcie is a lightweight, modular, single-header logging library written in C.
@@ -344,7 +344,7 @@
 // Versioning macros
 #define LOGCIE_VERSION_MAJOR         1
 #define LOGCIE_VERSION_MINOR         2
-#define LOGCIE_VERSION_RELEASE       0
+#define LOGCIE_VERSION_RELEASE       1
 #define LOGCIE_VERSION_NUMBER        (LOGCIE_VERSION_MAJOR * 100 * 100 + LOGCIE_VERSION_MINOR * 100 + LOGCIE_VERSION_RELEASE)
 #define LOGCIE_VERSION_FULL          LOGCIE_VERSION_MAJOR.LOGCIE_VERSION_MINOR.LOGCIE_VERSION_RELEASE
 #define LOGCIE_QUOTE(str)            #str
